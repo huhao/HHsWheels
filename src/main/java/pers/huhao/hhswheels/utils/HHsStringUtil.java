a61887f5cbe01e624a8f9cf6d1658b02654cb449
HH君 <hhlovedlh@gmail.com>
2023-11-16 19:10:50 +0800

    up

--- src/main/java/pers/huhao/hhswheels/utils/HHsStringUtil.java
+++ src/main/java/pers/huhao/hhswheels/utils/HHsStringUtil.java
@@ -4,7 +4,7 @@ import com.sun.istack.internal.Nullable;
 
 public class HHsStringUtil {
     static boolean isNullOrEmpty(@Nullable String value) {
-        return value == null || value.length() == 0;
+        return value == null || value.isEmpty();
     }
 
     static boolean isNotNullOrEmpty(@Nullable String value) {
